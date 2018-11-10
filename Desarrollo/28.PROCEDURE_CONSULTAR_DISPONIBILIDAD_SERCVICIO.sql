45fbacfe9ffb1222f865eff3d98531af2a012c05
nicolascensori <nicolascensori@gmail.com>
2018-11-10 19:25:39 -0300

    Modificación del WHERE

--- Desarrollo/28.PROCEDURE_CONSULTAR_DISPONIBILIDAD_SERCVICIO.sql
+++ Desarrollo/28.PROCEDURE_CONSULTAR_DISPONIBILIDAD_SERCVICIO.sql
@@ -1,7 +1,8 @@
 CREATE PROCEDURE consultar_disponibilidad_servicio
-	@nombre_servicio varchar(50)
+	@nombre_servicio varchar(50),
+	@fecha datetime
 AS BEGIN
-	IF EXISTS (SELECT * FROM Servicio s INNER JOIN Horarios_disponibles h ON s.id = h.id_servicio_profesional WHERE @nombre_servicio = s.nombre)
+	IF EXISTS (SELECT * FROM Servicio s INNER JOIN Horarios_disponibles h ON s.id = h.id_servicio_profesional WHERE @nombre_servicio = s.nombre AND CONVERT(date, h.fecha_hora_inicio) = CONVERT(date, @fecha))
 		PRINT 'El servicio esta disponible'
 	ELSE
 		PRINT 'El servicio NO esta disponible'
